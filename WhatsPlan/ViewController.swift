d4d414fd3715e1970e89eb5f73af13ad040dff12
이경민 <cow970814@naver.com>
2022-04-26 15:26:07 +0900

    Initial Commit

---
+++ WhatsPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsPlan
+//
+//  Created by 이경민 on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
